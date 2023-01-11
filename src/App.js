47402bc296f7cfd0d254ebe7e94acdcbc7052bdc
DVV <dimitar.vassilev@Gmail.com>
2023-01-11 22:42:16 +0200

    add russian

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Bye there!</p>
         <p>ДОСВИДАНИЯ!</p>
+        <p>Nashledanou!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
