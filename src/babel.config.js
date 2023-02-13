57f5dac528129af4f4824a8eb358cd70133658cd
DVV <dimitar.vassilev@Gmail.com>
2023-02-13 23:56:17 +0200

    more nodejs

--- src/babel.config.js
+++ src/babel.config.js
@@ -1,10 +1,12 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', {targets: {node: 'current'}}],
-    '@babel/preset-typescript',
-    '@babel/preset-react'
-  ],
-  plugins: [
-    '@babel/plugin-syntax-jsx'
-  ],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react',
+    ],
+    plugins: [
+      '@babel/plugin-syntax-jsx'
+    ]
+  };
 };
