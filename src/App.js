a861e91699d1142991c8fd97b7b8552693957f2f
DVV <dimitar.vassilev@Gmail.com>
2023-02-14 09:49:12 +0200

    switch to commonjs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// current
+//
+//----------------------------------------
+//import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+
+//class App extends Component {
+//  render() {
+//   return (
+//      <div className="App">
+//        <div className="App-header">
+//          <img src={logo} className="App-logo" alt="logo" />
+//          <h2>Welcome to React!</h2>
+//        </div>
+//        <p className="App-intro">
+//          bye there
+//        </p>
+//      </div>
+//    );
+//  }
+//}
+//
+//export default App;
+//-------------------cut-----------
+const React = require('react');
+const { Component } = React;
+const logo = require('./logo.svg');
+require('./App.css');
 
 class App extends Component {
   render() {
@@ -18,4 +44,5 @@ class App extends Component {
   }
 }
 
-export default App;
+module.exports = App;
+
