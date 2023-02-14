75be9d7d046b2a7362fc8803bc8e870c440c9340
DVV <dimitar.vassilev@Gmail.com>
2023-02-14 09:25:39 +0200

    rework test suite

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+//import React from 'react';
+//import ReactDOM from 'react-dom/client';
+//import App from './App';
+
+//it('renders without crashing', () => {
+//  const div = document.createElement('div');
+//  ReactDOM.createRoot(div).render(<App />);
+//});
+const React = require('react');
+const ReactDOM = require('react-dom');
+const App = require('./App').default;
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.createRoot(div).render(<App />);
+  ReactDOM.createRoot(div).render(React.createElement(App));
 });
